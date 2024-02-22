c981890c5c1cae8b220322420f9646987bcbfd0d
julia3657 <jhh16@mail.aub.edu>
2024-02-22 16:12:25 +0200

    Phase1

--- exchange/src/main/java/com/juliahaidarahmad/exchange/Controller.java
+++ exchange/src/main/java/com/juliahaidarahmad/exchange/Controller.java
@@ -3,12 +3,12 @@ package com.juliahaidarahmad.exchange;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
     }
 }
